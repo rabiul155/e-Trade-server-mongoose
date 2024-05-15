b44e0cbabc016253e64a3d8e74bf66ace8bce928
rabiul155 <siyamict@gamil.com>
2024-05-15 13:14:57 +0600

    fix cors issues

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-const productRoutes = require('./routes/productRoutes');
-const userRoutes = require('./routes/userRoutes');
-const cartsRoute = require('./routes/cartRoutes');
-
 //Middlewares
 app.use(express.json());
 app.use(cors());
+
+const productRoutes = require('./routes/productRoutes');
+const userRoutes = require('./routes/userRoutes');
+const cartsRoute = require('./routes/cartRoutes');
 
 //ROUTES
 app.use('/api/v1/products', productRoutes);
